0728ffd2db92c55a0f8893263f2e7196c7ae3ff4
Natan Moraes <natan.sbo@gmail.com>
2020-12-21 12:01:47 -0300

    Add handling for empty ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,13 +10,16 @@ const Burger = (props) => {
     .map((ingredientName) => {
       return [...Array(props.ingredients[ingredientName])].map((_, index) => {
         return <BurgerIngredient key={ingredientName + index} type={ingredientName} />
-      });
-    });
-
+      })
+    })
+    .reduce((finalArray, currentElement) => {
+      return finalArray.concat(currentElement);
+    }, []);
+  console.log(ingredients);
   return (
     <div id="burger">
       <BurgerIngredient type="bread-top" />
-      {ingredients}
+      {ingredients.length > 0 ? ingredients : <p>Please start adding ingredients!</p>}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
