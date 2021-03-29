9bafb73847f764951cf35ed5960956f36007ba35
Natan Moraes <natan.sbo@gmail.com>
2021-03-28 22:06:26 -0300

    Make orders page dynamic

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,21 @@ import React from 'react';
 import './Order.scss';
 
 const Order = (props) => {
+  const ingredients = [];
+
+  for (let name in props.ingredients) {
+    ingredients.push({
+      name: name,
+      amount: props.ingredients[name]
+    });
+  }
+
   return (
     <div className="order">
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>USD 1.99</strong></p>
+      <p>Ingredients: {ingredients.map(ingredient => (
+        <span className="ingredient" key={ingredient.name}>{ingredient.name} ({ingredient.amount})</span>
+      ))}</p>
+      <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
   );
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 export class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('orders.json')
+      .then(response => {
+        let orders = [];
+        for (let key in response.data) {
+          orders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: orders });
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div className="orders">
-        <Order />
-        <Order /> 
+        { this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
